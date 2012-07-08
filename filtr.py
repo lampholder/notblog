362807fe6ee9771e62ee7037b6f1d556cb46381e
Thomas Lant <lampholder@gmail.com>
2012-07-08 21:21:48 +0000

    a fairly sucky natural language processing filter

--- filtr.py
+++ filtr.py
@@ -1,5 +1,6 @@
 # This Python file uses the following encoding: utf-8
 import re
+import nltk
 
 swears = dict({"fuck":"frog", "shit":"shame", "cunt":"cone", "bollock":"balloon","anus":"angus", "rape":"ping", "raping":"damaging", "rapist":"mime", "dick":"baguette","twat":"hobbit", "arse":"frown", "wank":"shuffle", "crap":"craft", "penis":"wand", "cock":"pipe"})
 
@@ -19,3 +20,16 @@ def un_swear_word(word):
 def unswear(content):
     return ' '.join(map(lambda x: x if not is_swear_word(x) else un_swear_word(x), content.split()))
 
+def oddvar(html):
+    text = re.sub('<[^<]+?>', '', html)
+    pos_tag= nltk.pos_tag(nltk.word_tokenize(text))
+
+    print pos_tag
+    nouns = filter(lambda (x, y): y == 'NN', pos_tag)
+    nouns = map(lambda (x, y): x, nouns)
+    for noun in nouns:
+        try:
+            text = re.sub(noun, "Oddvar", text)                                                      
+        except:
+            a = 1
+    return text
